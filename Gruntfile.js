6fb5542fb770f64fc4f3242c77f6ce7f300688a2
Ajesh Shah <ajeshshah@gmail.com>
2015-04-21 19:47:09 -0700

    contrib-jade grunt and package json edit

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,16 @@ module.exports = function (grunt) {
     'use strict';
     // Project configuration
     grunt.initConfig({
+        jade: {
+            dist: {
+                options: {
+                    pretty: true
+                },
+                files: {
+                    'home.html': ['home.jade']
+                }
+            }
+        },
         watch: {
             gruntfile: {
                 files: '<%= jshint.gruntfile.src %>',
@@ -16,6 +26,7 @@ module.exports = function (grunt) {
 
     // These plugins provide necessary tasks
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jade');
 
     // Default task
     grunt.registerTask('default', ['jshint', 'qunit']);
