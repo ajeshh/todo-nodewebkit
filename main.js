5a79dfe632b3aa6326d814bc3ac8d5ef728f5547
Ajesh Shah <ajeshshah@gmail.com>
2015-04-21 20:16:02 -0700

    add jquery list to todo

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+$('.js-todo-form').on('submit', function(e) {
+    e.preventDefault();
+
+    var taskField = $(this).find('input[name="task"]');
+    var task = taskField.val();
+
+    taskField.val('');
+
+    $('.js-todo-list').append('<li>' + task + '</li>');
+});
